4f97f973d0a638e7c01ab9e51abe8983a84b1836
NongSnail <56298563+NongSnail@users.noreply.github.com>
2023-04-22 10:14:16 +0000

    [ADD] 00-bootcamp

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/extract.py
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/extract.py
@@ -2,10 +2,19 @@ import json
 
 import requests
 
+def write_json(new_data, filename='data.json'):
+    with open(filename, "r") as file:
+        data = json.load(file)
+    print(type(data)) 
+    data["list"].append(data)    
+    with open(filename, "w") as file:
+        json.dump(data, file)
 
 if __name__ == "__main__":
-    url = ""
+    url = "https://dog.ceo/api/breeds/image/random"
     response = requests.get(url)
     data = response.json()
+    del data['status']
+    print(data)
+    write_json(data)
 
-    # Your code here
